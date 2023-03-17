ea6337dac551171a23d8a683bc3c81e541bd8b50
radioactive99 <ag5193407@gmail.com>
2023-03-17 14:52:47 -0400

    building function convertCsvToTxt v-1.0.0

--- eltxtfiles/txtcreate/convertTxtBanesco.php
+++ eltxtfiles/txtcreate/convertTxtBanesco.php
@@ -32,19 +32,67 @@
 
 /**
  * 
- * name: convertCsvToTxtString
+ * name: convertCsvToTxt
  * @param string ruta al archivo conv ertir
  * @return N/A : escribe archivo xml
  * 
  */
-function convertCsvToTxtString($csv_string) {                                                   //|
+function convertCsvToTxt($csv_string) {                                                         //|
     $lines =  fopen($csv_string, 'rt');                                                         //|
     echo var_dump($lines);                                                                      //|
+    $cont = 0;               
+    $txt = fopen("txtCreated.txt", "w+");
+
 // ------------------------------------///////////////////////------------------------------------
     while(!feof($lines)){                                                                       //|
         $line = fgets($lines);                                                                  //|
-        $content = explode('#',$line);                                                          //|
-    }                                                                                           //|
+        $content = explode('#',$line); 
+        $valorcol1 = '';
+        if($cont>7){
+            $valorcol1 .= completar_codigo($content[2], 33, '<br>');
+            $valorcol1 .= completar_codigo($content[3], 48, '<br>');
+            $valorcol1 .= completar_codigo($content[4], 14, '<br>');
+            $valorcol1 .= completar_codigo($content[5], 17, '<br>');
+            $valorcol1 .= completar_codigo($content[6], 70, '<br>');
+            fwrite($txt,"\n");
+            fwrite($txt,$valorcol1);
+            // fwrite($txt,$content[2]."\n");
+
+            // fwrite($txt,$content[3]."\n");
+            // fwrite($txt,$content[4]."\n");
+            // fwrite($txt,$content[5]."\n");
+            // fwrite($txt,$content[6]."\n");
+            // fwrite($txt,$content[7]."\n");
+            // fwrite($txt,$content[8]."\n");
+        }
+
+        $cont+=1;
+    }            
+    fclose($txt);
+    echo $cont;                                                                               //|
 // ------------------------------------///////////////////////------------------------------------
-}                                                                                               //|
-convertCsvToXmlString("/home/general/Devel/codeigniter-eltxt/eltxtfiles/txtcreate/example.csv");                              //|
+}        
+
+/*
+* recibe un codigo y rellena con el caracter al inicio de la cadena
+* name: completar_codigo
+* @param string $incodigo
+* @return string
+*/
+function completar_codigo($incodigo ='', $digitos = 10, $caracter = ' ')
+{
+    if($digitos>10)
+        $digitos=10;
+    $excodigo=$incodigo;
+    $tam=strlen($incodigo);
+    if(strlen($incodigo)<$digitos)
+    {
+        $fin= $digitos -$tam ;
+        for ($i= 1; $i<= $fin; $i++ )
+            $excodigo= $caracter.$excodigo;
+    }
+    if(strlen($incodigo)>$digitos)
+        $excodigo=substr($incodigo,-1,$digitos);
+    return $excodigo;
+}//|
+convertCsvToTxt("/home/general/Devel/codeigniter-xmlislr/eltxtfiles/txtcreate/DataEntryenBsS1.csv");                              //|
