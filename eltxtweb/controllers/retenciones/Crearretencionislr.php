dc1ffc43ed2b0b32f9f498770b6b7a960b71b8e4
radioactive99 <ag5193407@gmail.com>
2023-03-24 10:50:05 -0400

    createRetencionIslr.

---
+++ eltxtweb/controllers/retenciones/Crearretencionislr.php
@@ -0,0 +1,74 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CrearRetencionISLR eltxt Controller Class para crear un de registro de retenciones
+ *
+ * @package     contabal
+ * @author      Lenz McKAY PICCORO @mckaygerhard
+ */
+class Crearretencionislr extends YA_Controller {
+
+	/**
+	 * name: desconocido
+	 * @param
+	 * @return
+	 */
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url','html'));
+		$this->output->enable_profiler(ENVIRONMENT !== 'production');
+
+	}
+
+
+	/**
+	 * index con menu
+	 *
+	 * @access	public
+	 * @param	void
+	 * @return	void
+	 */
+	public function index()
+	{
+
+		$data['menu'] = $this->genmenu();
+		$data['menusub'] = $this->genmenu('retenciones');
+		$this->render('reten_index',$data);
+	}
+
+	/**
+	 * entry point para vista formulario de crear retencion de ISLR
+	 *
+	 * @access	public
+	 * @param	void
+	 * @return	void
+	 */
+	public function registrarislr()
+	{
+
+		$data['menu'] = $this->genmenu();
+		$data['menusub'] = $this->genmenu('retenciones');
+		$this->render('reten_registrar_islr',$data);
+	}
+
+	/**
+	 * entry point para vista mostrar el comprobante de retencion de ISLR
+	 *
+	 * @access	public
+	 * @param	void
+	 * @return	void
+	 */
+	public function comprobanteislr($numerocomprobante = NULL)
+	{
+
+		$data['menu'] = $this->genmenu();
+		$data['menusub'] = $this->genmenu('tretencionesxts');
+		$this->render('reten_comprobante_islr',$data);
+	}
+
+
+}
+
+/* End of file CrearRetencionISLR.php */
+/* Location: ./eltxtweb/controllers/retenciones/CrearRetencionISLR.php */
