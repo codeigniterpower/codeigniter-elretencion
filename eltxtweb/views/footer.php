f679b173958a0f8887b0149ed65493b41aee1ef7
radioactive99 <ag5193407@gmail.com>
2023-03-29 09:49:47 -0400

    footer repaired

--- eltxtweb/views/footer.php
+++ eltxtweb/views/footer.php
@@ -3,8 +3,8 @@
 <!-- @author PICCORO Lenz MCKAY para el sistema codeigniter potenciado -->
 <?php
 echo br(1);
-echo '<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.'. PHP_EOL;
-echo br(1).anchor('http://venenux.blogspot.net','Sistema Codeigniter VNX').' es copyright &copy; 2012 PICCORO Lenz McKAY', PHP_EOL;
+echo '<p class="footer text-center">Page rendered in <strong>{elapsed_time}</strong> seconds.'. PHP_EOL;
+echo br(1).anchor('http://venenux.blogspot.net','Sistema Codeigniter VNX').' es copyright &copy; 2012 PICCORO Lenz McKAY And Radioactive Angel Gonzalez', PHP_EOL;
 echo '</p>'. PHP_EOL .'</center>', PHP_EOL;
 echo '</div>', PHP_EOL;
 echo '</body>', PHP_EOL;
