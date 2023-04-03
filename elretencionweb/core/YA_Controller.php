8f7aae3fb064a807ac4c0cd29934643279a34f36
mckaygerhard <mckaygerhard@gmail.com>
2023-04-03 18:02:41 -0400

    expanded the core controller that everyone inherits - part1
    * ampliado el controlador core que todos heredan
    * el menu es generado no importa que si hay o no sesion
    * la sesion no se detecta en el controlador, se espera un objeto
    * si la sesion objeto no se pasa o no existe se asume variables

--- elretencionweb/core/YA_Controller.php
+++ elretencionweb/core/YA_Controller.php
@@ -11,16 +11,16 @@ class YA_Controller extends CI_Controller
 {
 	/** inicia y en checku se asigna a la url pedida por get/post userurl */
 	public $userurl = NULL;
-    /** es la ruta actual de esta raiz del sistema */
-    public $currenturl = NULL;
-    /** es el nombre del inx indice de el url raiz (nombre del controler) */
-    public $currentctr = NULL;
-    /** es el nombre del sub indice de el url raiz (metodo del controler) */
-    public $currentinx = NULL;
-    /** si currenturl esta en un modulo, este es el nombre del modulo  */
-    public $currentmod = NULL;
-    /** collecion o arreglo de todos los elementos de el url RAIZ no del URL completo */
-    public $arraymurls = NULL;
+	/** es la ruta actual de esta raiz del sistema */
+	public $currenturl = NULL;
+	/** es el nombre del inx indice de el url raiz (nombre del controler) */
+	public $currentctr = NULL;
+	/** es el nombre del sub indice de el url raiz (metodo del controler) */
+	public $currentinx = NULL;
+	/** si currenturl esta en un modulo, este es el nombre del modulo  */
+	public $currentmod = NULL;
+	/** collecion o arreglo de todos los elementos de el url RAIZ no del URL completo */
+	public $arraymurls = NULL;
 	/**  permiso cargado en el controler en cda request */
 	public $permite = FALSE;
 	/**  nombre de usuario tomado de la session activa */
@@ -39,57 +39,50 @@ class YA_Controller extends CI_Controller
 
 		$this->load->helper(array('form', 'url','html'));
 		$this->load->library('table');
-//		$this->load->library('encrypt');
+		$this->load->library('encrypt');
 		$this->load->library('session');
 
-        $this->currentctr = $this->router->fetch_class();
-        $this->currentinx = $this->router->fetch_method();
+		$this->currentctr = $this->router->fetch_class();
+		$this->currentinx = $this->router->fetch_method();
 
-        $this->currenturl = $this->uri->uri_string();
-        $this->arraymurls = explode("/", $this->currenturl );
-        $this->currentmod = $this->arraymurls[0];
+		$this->currenturl = $this->uri->uri_string();
+		$this->arraymurls = explode("/", $this->currenturl );
+		$this->currentmod = $this->arraymurls[0];
 
 		if($modulo !== NULL AND trim($modulo) !== '')
 			$this->module = $modulo;
-        if(ENVIRONMENT !== 'production'){
-            $this->output->set_header("HTTP/1.1 200 OK");
-            $this->output->set_header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-            $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT');
-            $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
-            $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
-            $this->output->set_header("Pragma: no-cache");
-        }
 
-        if(ENVIRONMENT !== 'production')
-            $this->output->enable_profiler(TRUE);
+		if(ENVIRONMENT !== 'production'){
+			$this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT');
+			$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+			$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
+			$this->output->set_header("Pragma: no-cache");
+		}
+
+		if(ENVIRONMENT !== 'production')
+			$this->output->enable_profiler(TRUE);
 
 	}
 
-    /** revision de session, si invalidad redirige a login */
-    public function checku()
-    {
-        $this->userurl = $this->input->get_post('userurl');
-        $this->currenturl = $this->uri->uri_string(); //$this->uri->segment(1).'/'.$this->uri->segment(2).'/'.$this->uri->segment(3).'/'.$this->uri->segment(4);
+	/** revision de session, si invalidad redirige a login */
+	public function checku()
+	{
+		$this->userurl = $this->input->get_post('userurl');
+		$this->currenturl = $this->uri->uri_string(); //$this->uri->segment(1).'/'.$this->uri->segment(2).'/'.$this->uri->segment(3).'/'.$this->uri->segment(4);
 
 		$userurl = str_replace('/','',$this->userurl);
-        $redirurl = $this->currenturl;
+		$redirurl = $this->currenturl;
 		if( $userurl != '')
 			$redirurl = $this->userurl;
 
-        $username = $this->session->userdata('username');
-
 		$this->permite = $permited = $this->login->usercheck();
 		if( $permited != 1 OR $permited === FALSE)
-        {
-			// IMPORTANTE si se va a algo que llame a check o login, volvera aqui
-			if( $this->session->userdata('userlogin') == 1 )
-				redirect('indexlogin/entrarlogin?sinpermiso='.$redirurl,'location');
-			else
-				redirect('indexlogin/entrarlogin?userurl='.$redirurl,'location');
+		{
+			redirect('indexlogin/entrarlogin?userurl='.$redirurl,'location');
 		}
-        
+		
 		$this->username = $username;
-    }
+	}
 
 	/* 
 	 * la logica de menu esta descrita en docs/desarrollo-gencontroler-y-menu.md
@@ -98,87 +91,92 @@ class YA_Controller extends CI_Controller
 	 * @param string $moduledir nombre del directorio de controllers especifico sino directorios de modulos
 	 * @return string html table con los nombres de archivos de controladores o los directorios si no se especifica modulo dir
 	 */
-    public function genmenu($modulename = NULL)
-    {
-        $currentctr = $this->currentctr;
-        $currentinx = $this->currentinx;
-        $currenturl = $this->currenturl;
-        $arraymodules = $this->arraymurls;        
-        $arraycontrls = $this->getcontrollers($modulename);
+	public function genmenu($modulename = NULL, $userdata = NULL)
+	{
+		$currentctr = $this->currentctr;
+		$currentinx = $this->currentinx;
+		$currenturl = $this->currenturl;
+		$arraymodules = $this->arraymurls;        
+		$arraycontrls = $this->getcontrollers($modulename);
 
-        $user_loged = $this->session->userdata('username') OR TRUE;
-        $user_email = $this->session->userdata('useraddress') OR TRUE;
+		if( is_array($userdata) )
+		{
+			if( array_key_exists('username', $userdata) )
+				$user_loged = $userdata['username'] OR TRUE;
+			if( array_key_exists('username', $userdata) )
+				$user_email = $this->session->userdata('useraddress') OR TRUE;
+		}
+		else
+		{
+			$user_loged = FALSE;
+			$user_email = FALSE;
+		}
 
-        $menumainstring = '';
+		$menumainstring = '';
 
-        if(($modulename == NULL OR $modulename == FALSE) AND $currentinx !== '')
-        {
-            $menuitemactive = '';
-            $menuclasssubdi = '';
+		if(($modulename == NULL OR $modulename == FALSE) AND $currentinx !== '')
+		{
+			$menuitemactive = '';
+			$menuclasssubdi = '';
 
-            $menumainstring = '<div class="topnav '.$menuclasssubdi.'">';
-            $menumainstring .= ' '.anchor('../../','Intranet','class="" ');
-            $menumainstring .= ' '.anchor('/',ucfirst(APPCURREN),'class="active" ');
-            if( $user_email == NULL OR $user_loged == FALSE )
-            {
-                $menumainstring .= '</div>';
-                return $menumainstring;
-            }
-            else
-            {
-                $menumainstring .= anchor('',' ');
-                $modulename = $arraymodules[0];
-                foreach($arraycontrls as $menuidex=>$menulink)
-                {
-                    $menuitemactive = '';
-                    $findname = '/'.$modulename.'/';
-                    $menuname = preg_replace($findname, '', $menulink, 1);
-                    $menuclic = $menuname;
-                    $menuname = stristr($menuname,'index');
-                    $menuname = str_replace('index','',$menuname);
-                    $menuname = str_replace('_', ' ', $menuname);
-                    $menuname = str_replace('m', '', $menuname);
-                    $menuname = ucfirst($menuname);
-                    $menuname = ucwords($menuname);
-                    $menulink = strtolower($menulink);
-                    if(stripos($menulink,'/')>2)
-                    {
-                        if(stripos($menuclic,$modulename)>1)
-                            $menuitemactive = 'active';
-                        $menumainstring .= ' '.anchor($menulink,ucfirst($menuname),'class=" '.$menuitemactive.' " ');
-                    }
-                }
-                $menumainstring .= ' '.anchor('indexlogin/salirlogin','Salir','class="" ');
-            }
-        }
-        else
-        {
-            $menuclasssubdi = 'topnavsub';
-            $menumainstring .= '<div class="topnav '.$menuclasssubdi.'">';
-            foreach($arraycontrls as $menuidex=>$menulink)
-            {
-                $menuitemactive = '';
-                if(stripos($menulink,$currentctr)>1)
-                $menuitemactive = 'active';
-                if(stripos($menulink,'proc')>1)
-                    continue;
-                $findname = '/'.$modulename.'/';
-                $menuname = preg_replace($findname, '', $menulink, 1);
-                $menuname = str_replace('/','',$menuname);
-                $menuname = str_replace('m', '', $menuname);
-                if(stripos($menulink,'Index')>1)
-                $menuname = 'Recomenzar';
-                $menuname = ucfirst($menuname);
-                $menuname = str_replace('_', ' ', $menuname);
-                $menuname = ucwords($menuname);
-                $menulink = strtolower($menulink);
-                $menumainstring .= ' '.anchor($menulink,$menuname,'class="  '.$menuitemactive.' " ').' ';
-            }
-        }
-        $menumainstring .= '</div>';
-        return $menumainstring;
+			$menumainstring = '<div class="topnav '.$menuclasssubdi.'">';
+			$menumainstring .= ' '.anchor('../../','Intranet','class="" ');
+			if( $user_email == NULL OR $user_loged == FALSE )
+				$menumainstring .= ' '.anchor('/Index/vistainicio','Inicio','class="active" ');
+			else
+				$menumainstring .= ' '.anchor('/Index/vistasalida','Logout','class="active" ');
+
+			$modulename = $arraymodules[0];
+			foreach($arraycontrls as $menuidex=>$menulink)
+			{
+				$menuitemactive = '';
+				$findname = '/'.$modulename.'/';
+				$menuname = preg_replace($findname, '', $menulink, 1);
+				$menuclic = $menuname;
+				$menuname = stristr($menuname,'index');
+				$menuname = str_replace('index','',$menuname);
+				$menuname = str_replace('_', ' ', $menuname);
+				$menuname = str_replace('m', '', $menuname);
+				$menuname = ucfirst($menuname);
+				$menuname = ucwords($menuname);
+				$menulink = strtolower($menulink);
+				if(stripos($menulink,'/')>2)
+				{
+					if(stripos($menuclic,$modulename)>1)
+						$menuitemactive = 'active';
+					$menumainstring .= ' '.anchor($menulink,ucfirst($menuname),'class=" '.$menuitemactive.' " ');
+				}
+			}
+			$menumainstring .= ' '.anchor('/Index/vistasalida','Salir','class="" ');
+		}
+		else
+		{
+			$menuclasssubdi = 'topnavsub';
+			$menumainstring .= '<div class="topnav '.$menuclasssubdi.'">';
+			foreach($arraycontrls as $menuidex=>$menulink)
+			{
+				$menuitemactive = '';
+				if(stripos($menulink,$currentctr)>1)
+				$menuitemactive = 'active';
+				if(stripos($menulink,'proc')>1)
+					continue;
+				$findname = '/'.$modulename.'/';
+				$menuname = preg_replace($findname, '', $menulink, 1);
+				$menuname = str_replace('/','',$menuname);
+				$menuname = str_replace('m', '', $menuname);
+				if(stripos($menulink,'Index')>1)
+				$menuname = 'Recomenzar';
+				$menuname = ucfirst($menuname);
+				$menuname = str_replace('_', ' ', $menuname);
+				$menuname = ucwords($menuname);
+				$menulink = strtolower($menulink);
+				$menumainstring .= ' '.anchor($menulink,$menuname,'class="  '.$menuitemactive.' " ').' ';
+			}
+		}
+		$menumainstring .= '</div>';
+		return $menumainstring;
  
-    }
+	}
 
 	/* 
 	 * esta logica esta descrita en docs/desarrollo-gencontroler-y-menu.md
@@ -210,13 +208,13 @@ class YA_Controller extends CI_Controller
 		return $controllers;
 	}
 
-    /** permite repintar sin llamar tanto o escribir tanto, automaticamente carga header y footer */
-    public function render($view, $data = NULL) 
-    {
-        if( !isset($data['currenturl']) )
-            $data['currenturl'] = $this->currenturl;
-        if( !isset($data['userurl']) )
-            $data['userurl'] = $this->userurl;
+	/** permite repintar sin llamar tanto o escribir tanto, automaticamente carga header y footer */
+	public function render($view, $data = NULL) 
+	{
+		if( !isset($data['currenturl']) )
+			$data['currenturl'] = $this->currenturl;
+		if( !isset($data['userurl']) )
+			$data['userurl'] = $this->userurl;
 		$data['menu'] = $this->genmenu();
 		$this->seccion = uri_string();
 		$data['seccion'] = $this->seccion;
@@ -232,7 +230,7 @@ class YA_Controller extends CI_Controller
 				$this->load->view($vistacargar, $data);
 		}
 		$this->load->view('footer',$data);
-    }
+	}
 
 }
 
