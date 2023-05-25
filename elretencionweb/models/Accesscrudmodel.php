e2d80b265625e4f81f55cf8fc65dc80b99edb1a1
mckaygerhard <mckaygerhard@gmail.com>
2023-05-25 17:30:31 -0400

    access model - fix doc headers and add licence header

--- elretencionweb/models/Accesscrudmodel.php
+++ elretencionweb/models/Accesscrudmodel.php
@@ -1,16 +1,34 @@
 <?php 
 /**
+ * Copyright 2023 Angel Gonzalez Gmail:ag5193407@gmail.com , Gitlab 1:@Radioactive99
+ * Copyright 2023 PICCORO Lenz McKAY mckaygerhard
+ * 
+ * Usted es libre de:
+ * - Compartir — copiar y redistribuir el material en cualquier medio o formato
+ * - Adaptar — remezclar, transformar y crear a partir del material
+ * 
+ * El licenciador no puede revocar estas libertades mientras cumpla con los términos de la licencia.
+ * Bajo las condiciones siguientes:
+ * 
+ * - Reconocimiento — Debe reconocer adecuadamente la autoría, proporcionar un enlace a la licencia e indicar si se han realizado cambios<. Puede hacerlo de cualquier manera razonable, pero no de una manera que sugiera que tiene el apoyo del licenciador o lo recibe por el uso que hace.
+ * - NoComercial — No puede utilizar el material para una finalidad comercial.
+ * - CompartirIgual — Si remezcla, transforma o crea a partir del material, deberá difundir sus contribuciones bajo la misma licencia que el original.
+ * 
+ * No hay restricciones adicionales — No puede aplicar términos legales o medidas tecnológicas que legalmente restrinjan realizar aquello que la licencia permite.
+ */
+
+
+/**
  * Accesscrudmodel.php
  * 
- * abstraction DB para manejar tablas crudas en retenciondb
+ * abstraction DB para manejar acessos aparte de la clase de auth de autenticacion
  *
- * @author  PICCORO Lenz McKAY <mckaygerhard@gmail.com>
- * @copyright Copyright 2017
- * @version ab - 1.0
- * 
- * This program is free software; BUT you can redistribute it and/or modify
- * it under the terms of the CC-BY-NC-SA License
- * 
+ * @copyright Copyright 2023
+ * @package    CodeIgniter
+ * @category   Files
+ * @version    1.0.0-dev
+ * @author     PICCORO Lenz McKAY
+ * @link       https://gitlab.com/codeigniterpower/codeigniter-login
  */
 class Accesscrudmodel extends CI_Model 
 {
